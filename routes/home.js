914a2ff192fe87c0c0c292c9c22a4695cf83b93b
최영훈 <c_yh0327@naver.com>
2020-06-04 15:55:22 +0900

    keyword

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 const itemController = require('../controller/itemController');
 const homeController = require('../controller/homeController');
+const keywordController = require('../controller/keywordController');
 
 router.get('/recommend',itemController.recommendItem);
 router.get('/banner', homeController.getBanner);
 router.put('/like', itemController.toggleLike);
+router.get('/keyword', keywordController.readKeyword);
 
 module.exports = router;
